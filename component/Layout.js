d358312b73fda30dab9efd5b58ba456be3c7c13c
Julians22 <dabnerjulian@gmail.com>
2022-07-07 15:38:18 +0700

    update

--- component/Layout.js
+++ component/Layout.js
@@ -1,9 +1,21 @@
 import Navbar from './Navbar'
 import Footer from './Footer'
+import Head from 'next/head'
 
 export default function Layout({ children }) {
   return (
     <>
+      <Head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZYVB2KFQ5Q"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-ZYVB2KFQ5Q');
+        </script>
+      </Head>
       <Navbar />
         <main className='w-full p-3 md:p-0'>{children}</main>
       <Footer />
